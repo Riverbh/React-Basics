17081c285b183627ce68f59209a3e047f1c8009c
River Hansen <riverbh01@gmail.com>
2023-04-12 16:00:14 -0600

    added on a two-way binding

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 import Bravo from './Components/bravo';
 import Charlie from './Components/Charlie';
 
-let person = 'River'
+
 
 function App() {
+  const [userInput, setUserInput] = useState('')
+  const [name, setName] = useState('')
+
+  let person = 'River'
+
+  const handleChange = (event) => {
+    setUserInput(event.target.value)
+  }
+  const handleClick = () => {
+    setName(userInput)
+    setUserInput('')
+  }
   return (
     <div className="App">
-      <h1>App</h1>
+      <h1>App: {name}</h1>
+      <input placeholder="What's Your Name?" value={userInput} onChange={handleChange}/>
+      <button onClick={handleClick}>ChangeName</button>
       <Bravo personName={person}/> 
       <Bravo personName="Averie"/>
       <Charlie />
       <Charlie num={25}/>
       <Charlie num={75}/>
+      <Charlie num="75" /> 
     </div>
   );
 }
